03368d0ed76b9908c364d9d4da15d72a7b3d2a98
Anderson Garcia <andersongarcia@usp.br>
2023-10-22 21:12:19 -0300

    Fixing TestTouchTargetSize considering padding

--- accessibility-tests/src/main/java/br/usp/icmc/ppgccmc/accessibility_tests/mars/TestTouchTargetSize.java
+++ accessibility-tests/src/main/java/br/usp/icmc/ppgccmc/accessibility_tests/mars/TestTouchTargetSize.java
@@ -1,7 +1,5 @@
 package br.usp.icmc.ppgccmc.accessibility_tests.mars;
 
-import static org.junit.Assert.assertTrue;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,7 +7,6 @@ import android.widget.ImageButton;
 import org.junit.rules.ErrorCollector;
 
 import br.usp.icmc.ppgccmc.accessibility_tests.IAccessibilityTestViewHierarchy;
-import br.usp.icmc.ppgccmc.accessibility_tests.helpers.TestOutputHelper;
 
 public class TestTouchTargetSize implements IAccessibilityTestViewHierarchy {
     @Override
@@ -19,8 +16,23 @@ public class TestTouchTargetSize implements IAccessibilityTestViewHierarchy {
 
     @Override
     public void runTest(View view, ErrorCollector collector) {
+        int height = view.getHeight();
+        int width = view.getWidth();
+
+        // get padding left + right
+        int paddingLeft = view.getPaddingLeft();
+        int paddingRight = view.getPaddingRight();
+
+        // get padding top + bottom
+        int paddingTop = view.getPaddingTop();
+        int paddingBottom = view.getPaddingBottom();
+
+        // get height and width plus padding
+        int heightWithPadding = height + paddingTop + paddingBottom;
+        int widthWithPadding = width + paddingLeft + paddingRight;
+
         //assertTrue("Touch target size less then 48dp in : " + TestOutputHelper.getViewId(view), view.getHeight() < 48 || view.getWidth() < 48);
-        if(view.getHeight() < 48 || view.getWidth() < 48){
+        if(heightWithPadding < 48 || widthWithPadding < 48){
             collector.addError(new Throwable("Touch target size less then 48dp in : " + view));
         }
     }
